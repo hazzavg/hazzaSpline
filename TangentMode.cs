5c4a688ce9b83e7cc5fd6ea4a492a5724d2e54e8
Harry <46461093+hazzavg@users.noreply.github.com>
2021-03-09 13:11:51 +0000

    Update TangentMode.cs

--- TangentMode.cs
+++ TangentMode.cs
@@ -1,5 +1,10 @@
 ﻿using System;
 
+///<summary>
+/// This Script refers to the Various Instances in which the Spline can be Interpolated
+/// An Enum (Enumeration) refers to the Value Type that uses various Constants to determine a particular state
+/// This Script uses an Enum to determine the various States classified by the Spline
+///<summary>
 namespace hazza.Splines
 {
 	[Serializable] // Serializable refers to how other Scripts are able to Access this Class/Script
